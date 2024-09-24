e2176d59d0170b761459dd2bb197b5c11d770f15
Evan Baldrick <Baldrick-E@ulster.ac.uk>
2024-09-24 14:48:02 +0100

    Add project files.

---
+++ CalcApp/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Type in first number followed by the Enter key");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Type in second number followed by the Enter key");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+//perform the calculation
+int result = firstNumber + secondNumber;
+
+//output answer to console
+Console.WriteLine("Adding {0} and {1} give the answer {2}", firstNumber, secondNumber, result);
